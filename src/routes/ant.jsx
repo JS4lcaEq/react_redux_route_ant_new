a4a10d1435e4155058496f436353d3baa789e11f
Vasilev Andrey <condorrr@gmail.com>
2023-09-11 02:20:28 +0300

    init2

--- src/routes/ant.jsx
+++ src/routes/ant.jsx
@@ -5,19 +5,19 @@ export default function Ant() {
     const dig = (path = '0', level = 3) => {
         const list = [];
         for (let i = 0; i < 10; i += 1) {
-          const key = `${path}-${i}`;
-          const treeNode = {
-            title: key,
-            key,
-          };
-          if (level > 0) {
-            treeNode.children = dig(key, level - 1);
-          }
-          list.push(treeNode);
+            const key = `${path}-${i}`;
+            const treeNode = {
+                title: key,
+                key,
+            };
+            if (level > 0) {
+                treeNode.children = dig(key, level - 1);
+            }
+            list.push(treeNode);
         }
         return list;
-      };
-      const treeData = dig();
+    };
+    const treeData = dig();
 
     return (
         <>
@@ -26,7 +26,7 @@ export default function Ant() {
                 <DatePicker />
             </div>
             <div>
-            <Tree treeData={treeData} height={233} defaultExpandAll className="tree-box" />
+                <Tree treeData={treeData} height={233} defaultExpandAll className="tree-box" />
             </div>
         </>
 
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -3,13 +3,7 @@ export default function Root() {
     return (
       <>
         <h1>Root</h1>
-        <a href={`/`}>Index</a> 
-        || 
-        <a href={`/contacts/2`}>contacts</a> 
-        ||  
-        <a href={`/counter`}>counter</a>
-        ||
-        <a href={`/ant`}>ant</a>
+        <a href={`/`}>Index</a> &nbsp; <a href={`/contacts/2`}>contacts</a> &nbsp; <a href={`/counter`}>counter</a> &nbsp; <a href={`/ant`}>ant</a>
         <div>
             <Outlet />
         </div>
